e4bc0b7a262a24957afa06e847a3016f7fc2ccc0
karthikodc <k.raja@accenture.com>
2012-10-11 18:15:53 +0530

    Update examples/jive-core/search-content/search-content.js

--- examples/jive-core/search-content/search-content.js
+++ examples/jive-core/search-content/search-content.js
@@ -65,6 +65,24 @@ function search() {
             });
             
             html +="<hr>";
+            .each(rows, function(index, row) {   
+               if(row.type=="blog")
+               {
+                     url=row.resources.html.ref;
+                     subject=row.subject;
+                      contentSummary=row.contentSummary;
+                      author=row.author.name;
+                      modifiedDate=row.modificationDate;
+                     likeCount=row.likeCount;
+                     type=row.type;
+                     html +="<div>";
+                     html +="<ul><li>"+author+" "+subject+" "+type+"</li><ul>"
+                     html +="</div>"
+                  
+               }
+            });
+            
+            html +="<hr>";
             $.each(rows, function(index, row) {   
                if(row.type=="discussion")
                {
