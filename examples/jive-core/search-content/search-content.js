b9ffcbfc870fb6ed2d9d83970659a4134f412def
karthikodc <k.raja@accenture.com>
2012-10-11 15:19:44 +0530

    Update examples/jive-core/search-content/search-content.js

--- examples/jive-core/search-content/search-content.js
+++ examples/jive-core/search-content/search-content.js
@@ -23,7 +23,29 @@ function search() {
         
         
     };
+function getISOStrict(date) {
 
+    if (Date.prototype.toISOString) {
+        return date.toISOString().replace(/Z$/, "+0000");
+    }
+
+    function pad(number) {
+        var r = String(number);
+        if ( r.length === 1 ) {
+            r = '0' + r;
+        }
+        return r;
+    }
+
+    return date.getUTCFullYear()
+        + '-' + pad( date.getUTCMonth() + 1 )
+        + '-' + pad( date.getUTCDate() );
+       // + 'T' + pad( date.getUTCHours() )
+       // + ':' + pad( date.getUTCMinutes() )
+      //  + ':' + pad( date.getUTCSeconds() )
+       // + '.' + String( (date.getUTCMilliseconds()/1000).toFixed(3) ).slice( 2, 5 )
+   //     + '+0000';
+}
    
    /* if (types.length > 0) {
         params.type = types;
@@ -40,6 +62,8 @@ function search() {
             var html = "";
             var rows = response.data;
             $.each(rows, function(index, row) {
+               var date = new Date(row.modificationDate);
+               var newdate=function getISOStrict(date);
                 html += "<tr colspan='3'>";
                 html += "<td>" + row.type + "</td>";
                 html += "<td><a href="+ row.resources.html.ref +">" + row.subject + "</a></td>";
@@ -50,7 +74,7 @@ function search() {
                html += "<tr >";
                html += "<td>Author:</td> <td><img src=" + row.author.avatarURL + " width='25px' height='25px' border='0'/></td><td><a href=https://apps.onprem.jivesoftware.com/people/"+row.author.username+">" + row.author.name + "</td>";
                 html += "<td>Likes:</td><td>" + row.likeCount + "</td>";
-                html += "<td>Modification Date:</td><td>" + row.modificationDate + "</td>";
+                html += "<td>Modification Date:</td><td>" + newdate + "</td>";
                html += "</tr>";
                html += "</table>";
                 html += "</td></tr>";
